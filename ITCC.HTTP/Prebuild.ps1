b7f113f6c4385e39e504f295c62faaa35a2d751f
Vladimir Tyrin <b0-0b@yandex.ru>
2016-05-03 14:09:16 +0300

    Correct prebuild

---
+++ ITCC.HTTP/Prebuild.ps1
@@ -0,0 +1,16 @@
+﻿param(
+	[string]$SolutionDir,
+	[string]$SolutionPath
+)
+
+Write-Host "Started prebuild"
+Write-Host "Solution directory $SolutionDir"
+Write-Host "Solution path: $SolutionPath"
+$NugetPath = "$SolutionDir\nuget.exe"
+if (Test-Path "$NugetPath") {
+  
+} else {
+	(New-Object Net.WebClient).DownloadFile('https://nuget.org/nuget.exe', "$NugetPath")
+}
+& "$NugetPath" restore $SolutionPath
+Write-Host "Finished prebuild"
