d6d4b3d9a2d819fd1d8c453dd4f46e839b485c92
Vladimir Tyrin <b0-0b@yandex.ru>
2016-06-15 10:05:55 +0300

    RequestCancelled condition fixed

--- ITCC.HTTP/Client/RegularClient.cs
+++ ITCC.HTTP/Client/RegularClient.cs
@@ -218,7 +218,7 @@ namespace ITCC.HTTP.Client
             }
             catch (OperationCanceledException ocex)
             {
-                if (ocex.CancellationToken.Equals(cancellationToken))
+                if (cancellationToken.IsCancellationRequested)
                 {
                     LogMessage(LogLevel.Debug, $"Request {method.Method} /{partialUri} has been cancelled");
                     return new RequestResult<TResult>
