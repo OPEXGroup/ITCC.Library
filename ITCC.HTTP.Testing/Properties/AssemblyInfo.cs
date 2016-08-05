4982e4f7c49fe158d24325d2410296db894ba010
Vladimir Tyrin <b0-0b@yandex.ru>
2016-08-05 11:27:23 +0300

    Project renamed

--- ITCC.HTTP.Testing/Properties/AssemblyInfo.cs
+++ ITCC.HTTP.Testing/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ITCC.Library.Testing")]
+[assembly: AssemblyTitle("ITCC.HTTP.Testing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
