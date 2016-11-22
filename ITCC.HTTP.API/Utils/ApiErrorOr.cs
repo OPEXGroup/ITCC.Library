ac42172469b6fc0f2cb514c471ae6040e92e9e4a
Vladimir.Tyrin <b0-0b@yandex.ru>
2016-11-22 15:00:46 +0300

    A couple of util classes added

---
+++ ITCC.HTTP.API/Utils/ApiErrorOr.cs
@@ -0,0 +1,21 @@
+﻿namespace ITCC.HTTP.API.Utils
+{
+    public class ApiErrorOr<T> : Either<ApiErrorView, T>
+        where T : class
+    {
+        #region public
+        public static ApiErrorOr<T> Error(ApiErrorView errorView) => new ApiErrorOr<T>(errorView);
+        public static ApiErrorOr<T> Success(T data) => new ApiErrorOr<T>(data);
+
+        public ApiErrorView ErrorView => First;
+        public bool IsError => ErrorView != null;
+        public T Value => Second;
+        public bool IsSuccess => !IsError;
+        #endregion
+
+        #region private
+        private ApiErrorOr(ApiErrorView errorView) : base(errorView) { }
+        private ApiErrorOr(T data) : base(data) { }
+        #endregion
+    }
+}
---
+++ ITCC.HTTP.API/Utils/Either.cs
@@ -0,0 +1,25 @@
+﻿namespace ITCC.HTTP.API.Utils
+{
+    public class Either<TFirst, TSecond>
+        where TFirst : class
+        where TSecond : class
+    {
+        public Either(TFirst first)
+        {
+            First = first;
+        }
+
+        public Either(TSecond second)
+        {
+            Second = second;
+        }
+
+        private Either() { }
+
+        public bool HasFirst => First != null;
+        public bool HasSecond => Second != null;
+
+        public TFirst First { get; }
+        public TSecond Second { get; }
+    }
+}
