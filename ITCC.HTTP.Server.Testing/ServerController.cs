c4fb8160549d745dc0b3354f32287eaabbb3f581
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-01-18 05:55:31 +0300

    Cleanup

--- ITCC.HTTP.Server.Testing/ServerController.cs
+++ ITCC.HTTP.Server.Testing/ServerController.cs
@@ -2,13 +2,9 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Serialization;
 using ITCC.HTTP.Server.Core;
 using ITCC.HTTP.Server.Encoders;
 using ITCC.HTTP.Server.Enums;
@@ -17,7 +13,6 @@ using ITCC.HTTP.Server.Interfaces;
 using ITCC.HTTP.Server.Testing.Utils;
 using ITCC.HTTP.SslConfigUtil.Core.Enums;
 using ITCC.Logging.Core;
-using Newtonsoft.Json;
 
 namespace ITCC.HTTP.Server.Testing
 {
@@ -47,47 +42,44 @@ namespace ITCC.HTTP.Server.Testing
 
         public static void Stop() => StaticServer<AccountMock>.Stop();
 
-        private static HttpServerConfiguration<AccountMock> GetConfig()
+        private static HttpServerConfiguration<AccountMock> GetConfig() => new HttpServerConfiguration<AccountMock>
         {
-            return new HttpServerConfiguration<AccountMock>
+            Port = 8888,
+            Protocol = Configuration.Protocol,
+            AllowGeneratedCertificates = true,
+            CertificateBindType = BindType.SubjectName,
+            LogBodyReplacePatterns = new List<Tuple<string, string>>
             {
-                Port = 8888,
-                Protocol = Configuration.Protocol,
-                AllowGeneratedCertificates = true,
-                CertificateBindType = BindType.SubjectName,
-                LogBodyReplacePatterns = new List<Tuple<string, string>>
+                new Tuple<string, string>("(\"Token\":\")([\\w\\d]+)(\")", "$1REMOVED_FROM_LOG$3")
+            },
+            LogProhibitedQueryParams = new List<string> {"password"},
+            LogProhibitedHeaders = new List<string> {"Authorization"},
+            ServerName = "ITCC Test",
+            StatisticsEnabled = true,
+            SubjectName = "localhost",
+            FilesEnabled = true,
+            FilesNeedAuthorization = false,
+            FilesBaseUri = "files",
+            FileSections = new List<FileSection>
+            {
+                new FileSection
                 {
-                    new Tuple<string, string>("(\"Token\":\")([\\w\\d]+)(\")", "$1REMOVED_FROM_LOG$3")
-                },
-                LogProhibitedQueryParams = new List<string> {"password"},
-                LogProhibitedHeaders = new List<string> {"Authorization"},
-                ServerName = "ITCC Test",
-                StatisticsEnabled = true,
-                SubjectName = "localhost",
-                FilesEnabled = true,
-                FilesNeedAuthorization = false,
-                FilesBaseUri = "files",
-                FileSections = new List<FileSection>
-                {
-                    new FileSection
-                    {
-                        Folder = "Test",
-                        MaxFileSize = -1,
-                        Name = "Test"
-                    }
-                },
-                FilesLocation = Configuration.FilesLocation,
-                FilesPreprocessingEnabled = true,
-                FilesCompressionEnabled = true,
-                FilesPreprocessorThreads = -1,
-                BodyEncoders = new List<IBodyEncoder>
-                {
-                    new PlainTextBodyEncoder(),
-                    new JsonBodyEncoder(isDefault: true),
-                    new XmlBodyEncoder()
-                },
-                CriticalMemoryValue = 1024
-            };
-        }
+                    Folder = "Test",
+                    MaxFileSize = -1,
+                    Name = "Test"
+                }
+            },
+            FilesLocation = Configuration.FilesLocation,
+            FilesPreprocessingEnabled = true,
+            FilesCompressionEnabled = true,
+            FilesPreprocessorThreads = -1,
+            BodyEncoders = new List<IBodyEncoder>
+            {
+                new PlainTextBodyEncoder(),
+                new JsonBodyEncoder(isDefault: true),
+                new XmlBodyEncoder()
+            },
+            CriticalMemoryValue = 1024
+        };
     }
 }
