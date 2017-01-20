b72897b309ae3e9f71e2b99c46d9ba3b4fc61f3b
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-01-20 07:29:48 +0300

    IAsyncCommand and DelegateCommand added

---
+++ ITCC.WPF/Commands/DelegateCommand.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ITCC.WPF.Commands
+{
+    public class DelegateCommand : ICommand, INotifyPropertyChanged
+    {
+        #region ICommand
+        public bool CanExecute(object parameter)
+        {
+            var value = _executionCondition.Invoke();
+            Enabled = value;
+            return value;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (_hasParam)
+                _parameterizedCommand(parameter);
+            else
+                _command();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion
+
+        #region public
+
+        public DelegateCommand(Action command, Func<bool> executionCondition = null)
+        {
+            _command = command;
+            if (executionCondition != null)
+                _executionCondition = executionCondition;
+        }
+        public DelegateCommand(Action<object> command, Func<bool> executionCondition = null)
+        {
+            _hasParam = true;
+            _parameterizedCommand = command;
+            if (executionCondition != null)
+                _executionCondition = executionCondition;
+        }
+
+        public bool Enabled
+        {
+            get { return _enabled; }
+            private set
+            {
+                _enabled = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region private
+
+        private readonly Action _command;
+        private readonly Action<object> _parameterizedCommand;
+        private readonly Func<bool> _executionCondition = () => true;
+        private readonly bool _hasParam;
+        private bool _enabled;
+
+        #endregion
+    }
+}
---
+++ ITCC.WPF/Interfaces/IAsyncCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ITCC.WPF.Interfaces
+{
+    /// <summary>
+    ///     Used for WPF bindings. 
+    ///     Used for asynchronous commands with possible asynchronous cancellation support.
+    /// </summary>
+    public interface IAsyncCommand : ICommand
+    {
+        Task ExecuteAsync(object parameter);
+    }
+}
