a8e164a9cdf9cbccd52fe4a1ecae9eca05bc86a9
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-03-15 17:50:23 +0300

    Missing annotations

--- src/ITCC.HTTP.API/Utils/Empty.cs
+++ src/ITCC.HTTP.API/Utils/Empty.cs
@@ -1,5 +1,13 @@
-﻿namespace ITCC.HTTP.API.Utils
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+namespace ITCC.HTTP.API.Utils
 {
+    /// <summary>
+    ///     Should be used as the only constructor argument in
+    ///     <see cref="Attributes.ApiRequestBodyTypeAttribute"/> and <see cref="Attributes.ApiResponseBodyTypeAttribute"/>
+    ///     to indicate empty bodies
+    /// </summary>
     public abstract class Empty
     {
 
