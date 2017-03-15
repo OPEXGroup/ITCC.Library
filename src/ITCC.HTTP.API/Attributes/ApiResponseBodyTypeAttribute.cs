32f1d37afd5817466de5126195f22f0ab132d9c3
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-03-15 17:13:50 +0300

    Type attributes implemented

--- src/ITCC.HTTP.API/Attributes/ApiResponseBodyTypeAttribute.cs
+++ src/ITCC.HTTP.API/Attributes/ApiResponseBodyTypeAttribute.cs
@@ -2,11 +2,49 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 using System;
+using ITCC.HTTP.API.Interfaces;
 
 namespace ITCC.HTTP.API.Attributes
 {
+    /// <summary>
+    ///     Describes api response body format
+    ///     Must ONLY be used with properties implementing <see cref="Common.Interfaces.IRequestProcessor"/>
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
-    public class ApiResponseBodyTypeAttribute : Attribute
+    public class ApiResponseBodyTypeAttribute : Attribute, ITypeAttribute
     {
+        #region construction
+
+        public ApiResponseBodyTypeAttribute(Type type1)
+        {
+            Type1 = type1;
+        }
+
+        public ApiResponseBodyTypeAttribute(Type type1, Type type2)
+            : this(type1)
+        {
+            Type2 = type2;
+        }
+
+        public ApiResponseBodyTypeAttribute(Type type1, Type type2, Type type3)
+            : this(type1, type2)
+        {
+            Type3 = type3;
+        }
+
+        public ApiResponseBodyTypeAttribute(Type type1, Type type2, Type type3, Type type4)
+            : this(type1, type2, type3)
+        {
+            Type4 = type4;
+        }
+
+        #endregion
+
+        #region properties
+        public Type Type1 { get; }
+        public Type Type2 { get; }
+        public Type Type3 { get; }
+        public Type Type4 { get; }
+        #endregion
     }
 }
---
+++ src/ITCC.HTTP.API/Utils/TypeAttributeHelper.cs
@@ -0,0 +1,34 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using System;
+using System.Collections.Generic;
+using ITCC.HTTP.API.Interfaces;
+
+namespace ITCC.HTTP.API.Utils
+{
+    internal static class TypeAttributeHelper
+    {
+        public static List<Type> GetTypes(this ITypeAttribute attribute)
+        {
+            if (attribute == null)
+                throw new ArgumentNullException(nameof(attribute));
+
+            var result = new List<Type>();
+
+            if (attribute.Type1 != null)
+                result.Add(attribute.Type1);
+
+            if (attribute.Type2 != null)
+                result.Add(attribute.Type2);
+
+            if (attribute.Type3 != null)
+                result.Add(attribute.Type3);
+
+            if (attribute.Type4 != null)
+                result.Add(attribute.Type4);
+
+            return result;
+        }
+    }
+}
