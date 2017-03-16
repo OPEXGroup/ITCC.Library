44592425f531a1496e07c8dbf8d9c694d7e7b1a3
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-03-16 14:11:50 +0300

    Empty test views added

---
+++ tests/ITCC.HTTP.Api.Documentation.Testing/Enums/SecondEnum.cs
@@ -0,0 +1,9 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+namespace ITCC.HTTP.Api.Documentation.Testing.Enums
+{
+    public enum SecondEnum
+    {
+    }
+}
---
+++ tests/ITCC.HTTP.Api.Documentation.Testing/Views/FirstRootView.cs
@@ -0,0 +1,13 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using ITCC.HTTP.API.Attributes;
+using ITCC.HTTP.API.Enums;
+
+namespace ITCC.HTTP.Api.Documentation.Testing.Views
+{
+    [ApiView(ApiHttpMethod.Get)]
+    public class FirstRootView
+    {
+    }
+}
