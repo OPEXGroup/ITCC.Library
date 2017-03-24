755a48d112d9ce6977414b63ef71e3d6753009c7
Vladimir.Tyrin <b0-0b@yandex.ru>
2017-03-24 15:47:17 +0300

    Better enum infos

--- src/ITCC.HTTP.API/Enums/ApiHttpMethod.cs
+++ src/ITCC.HTTP.API/Enums/ApiHttpMethod.cs
@@ -1,38 +1,45 @@
 ﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 using System;
+using ITCC.HTTP.API.Attributes;
 
 namespace ITCC.HTTP.API.Enums
 {
     /// <summary>
-    ///     Used in <see cref="Attributes.ApiViewAttribute"/> to indicate proper usage
+    ///     Used in <see cref="ApiViewAttribute"/> to indicate proper usage
     /// </summary>
     [Flags]
     public enum ApiHttpMethod
     {
         /// <summary>
-        ///     View is used only in responses for non-get methods
+        ///     Default value
         /// </summary>
+        [EnumValueInfo(@"Default value")]
         Default = 0,
         /// <summary>
         ///     HTTP GET
         /// </summary>
+        [EnumValueInfo(@"HTTP GET")]
         Get = 1 << 0,
         /// <summary>
         ///     HTTP POST
         /// </summary>
+        [EnumValueInfo(@"HTTP POST")]
         Post = 1 << 1,
         /// <summary>
         ///     HTTP PUT
         /// </summary>
+        [EnumValueInfo(@"HTTP PUT")]
         Put = 1 << 2,
         /// <summary>
-        ///     This SHOULD NOT be used. Use guids instead to remove entities
+        ///     HTTP DELETE. This SHOULD NOT be used. Use guids instead to remove entities
         /// </summary>
+        [EnumValueInfo(@"HTTP DELETE")]
         Delete = 1 << 3,
         /// <summary>
         ///     HTTP PATCH
         /// </summary>
+        [EnumValueInfo(@"HTTP PATCH")]
         Patch = 1 << 4
     }
 }
---
+++ src/ITCC.HTTP.API/Enums/ApiMethodState.cs
@@ -0,0 +1,41 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using System;
+using ITCC.HTTP.API.Attributes;
+
+namespace ITCC.HTTP.API.Enums
+{
+    /// <summary>
+    ///     
+    /// </summary>
+    [Flags]
+    public enum ApiMethodState
+    {
+        /// <summary>
+        ///     Default method value. This value is meant when no additional info is provided
+        /// </summary>
+        [EnumValueInfo("Unspecified")]
+        Unspecified = 1 << 0,
+        /// <summary>
+        ///     Some of method intended features do not work at the moment. Additional info should be provided
+        /// </summary>
+        [EnumValueInfo("Works partially")]
+        WorksPartially = 1 << 1,
+        /// <summary>
+        ///     Method call will match the description
+        /// </summary>
+        [EnumValueInfo("Works fine")]
+        WorksFine = 1 << 2,
+        /// <summary>
+        ///     Method works with bugs. Known bugs should be listed in additional description
+        /// </summary>
+        [EnumValueInfo("Works with bugs")]
+        WorksWithBugs = 1 << 3,
+        /// <summary>
+        ///     Method has not been implemented yet
+        /// </summary>
+        [EnumValueInfo("Not implemented")]
+        NotImplemented = 1 << 4,
+    }
+}
