4a755f37eb0b161b841a9ec952093f797792eb5b
Vladimir Tyrin <b0-0b@yandex.ru>
2017-03-28 14:50:08 +0300

    Prebuild scripts fixed to use latest nuget

--- src/ITCC.Geocoding/Prebuild.ps1
+++ src/ITCC.Geocoding/Prebuild.ps1
@@ -2,17 +2,26 @@
 	[string]$SolutionDir
 )
 
+$LibraryName = "ITCC.Library"
+$ProjectName = "ITCC.Geocoding"
+
 Write-Host "Started prebuild"
 Write-Host "Solution directory $SolutionDir"
+
 $NugetPath = "$SolutionDir\nuget.exe"
+
 if (Test-Path "$NugetPath") {
-  
+	Write-Host "Nuget.exe has been already downloaded"
 } else {
-	(New-Object Net.WebClient).DownloadFile('https://nuget.org/nuget.exe', "$NugetPath")
+	(New-Object Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', "$NugetPath")
 }
-if ("$SolutionDir".Contains("ITCC.Library")) {
+if ("$SolutionDir".Contains("$LibraryName")) {
 	Write-Host "Nothing to do"
 } else {
-	& "$NugetPath" restore "$SolutionDir\lib\ITCC.Library\src\ITCC.Geocoding\packages.config" -OutputDirectory "$SolutionDir\lib\ITCC.Library\packages"
+	$PackagesConfigPath = "$SolutionDir\lib\$LibraryName\src\$ProjectName\packages.config"
+	$PackagesBinariesPath = "$SolutionDir\lib\$LibraryName\packages\"
+	Write-Host "packages.config file $PackagesConfigPath"
+	Write-Host "packages binaries directory $PackagesBinariesPath"
+	& "$NugetPath" restore "$PackagesConfigPath" -OutputDirectory "$PackagesBinariesPath"
 }
 Write-Host "Finished prebuild"
