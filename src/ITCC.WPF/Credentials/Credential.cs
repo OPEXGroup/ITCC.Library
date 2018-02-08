351a5d7b3686a04fbc3b294694172d8b08330e9e
Vladimir Tyrin <b0-0b@yandex.ru>
2018-02-08 18:31:19 +0300

    Credential processing in ITCC.WPF

---
+++ src/ITCC.WPF/Credentials/Credential.cs
@@ -0,0 +1,28 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+using ITCC.WPF.Enums;
+
+namespace ITCC.WPF.Credentials
+{
+    /// <summary>
+    ///     Represents Windows application credential
+    /// </summary>
+    public class Credential
+    {
+        public CredentialType CredentialType { get; }
+        public string ApplicationName { get; }
+        public string UserName { get; }
+        public string Password { get; }
+
+        public Credential(CredentialType credentialType, string applicationName, string userName, string password)
+        {
+            ApplicationName = applicationName;
+            UserName = userName;
+            Password = password;
+            CredentialType = credentialType;
+        }
+
+        public override string ToString() => $"CredentialType: {CredentialType}, ApplicationName: {ApplicationName}, UserName: {UserName}, Password: {Password}";
+    }
+}
---
+++ src/ITCC.WPF/Enums/CredentialPersintence.cs
@@ -0,0 +1,14 @@
+﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+namespace ITCC.WPF.Enums
+{
+    /// <summary>
+    ///     Credential persistence type
+    /// </summary>
+    public enum CredentialPersistence : uint
+    {
+        Session = 1,
+        LocalMachine,
+        Enterprise
+    }
+}
